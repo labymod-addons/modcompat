f8d712cedb22ba0f5c721c32794cdff321190910
Julian <42209193+juliarn@users.noreply.github.com>
2024-03-29 19:38:51 +0100

    Fix 1.19.4 modmenu mixin remap issue

--- mod-compatibility/modmenu/build.gradle.kts
+++ mod-compatibility/modmenu/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     // TODO: instead of files(), use remap() and fetch from the modrinth repo
     v1_19_2CompileOnly(files("./libs/modmenu-1.19.3.jar"))
     v1_19_3CompileOnly(files("./libs/modmenu-1.19.3.jar"))
-    v1_19_4CompileOnly(files("./libs/modmenu-1.20.2.jar"))
+    v1_19_4CompileOnly(files("./libs/modmenu-1.19.4.jar"))
     v1_20_1CompileOnly(files("./libs/modmenu-1.20.2.jar"))
     v1_20_2CompileOnly(files("./libs/modmenu-1.20.2.jar"))
     v1_20_4CompileOnly(files("./libs/modmenu-1.20.4.jar"))
