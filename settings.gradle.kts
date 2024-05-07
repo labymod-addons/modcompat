316a38f503b794162fe5e540e242445bf519c6d7
Julian <montorrojulian@gmail.com>
2024-05-07 21:36:24 +0200

    Bump labygradle to fix mixin refmap override

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "modcompat"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.48"
+    val labyGradlePluginVersion = "0.3.49"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
