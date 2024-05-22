a7ba38a05805f0b8834eaea67fea72df1f0103ef
Julian <42209193+juliarn@users.noreply.github.com>
2024-05-23 00:49:57 +0200

    Fix apoli

---
+++ mod-compatibility/apoli/src/main/java/net/labymod/addons/modcompat/apoli/transformer/LivingEntityMixinTransformer.java
@@ -0,0 +1,33 @@
+package net.labymod.addons.modcompat.apoli.transformer;
+
+import net.labymod.addons.modcompat.transformer.MixinClassTransformer;
+import net.labymod.api.models.addon.annotation.EarlyAddonTransformer;
+import org.objectweb.asm.tree.AnnotationNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+
+@EarlyAddonTransformer
+public class LivingEntityMixinTransformer extends MixinClassTransformer {
+
+  private static final String MIXIN_METHOD_NAME = "modifySlipperiness";
+
+  public LivingEntityMixinTransformer() {
+    super("io.github.apace100.apoli.mixin.LivingEntityMixin");
+  }
+
+  @Override
+  protected void transform(ClassNode classNode) {
+    for (MethodNode method : classNode.methods) {
+      if (MIXIN_METHOD_NAME.equals(method.name) && method.visibleAnnotations != null) {
+        for (AnnotationNode visibleAnnotation : method.visibleAnnotations) {
+          if (MODIFY_VARIABLE_DESC.equals(visibleAnnotation.desc)) {
+            visibleAnnotation.values.add("ordinal");
+            visibleAnnotation.values.add(0);
+            break;
+          }
+        }
+        break;
+      }
+    }
+  }
+}
