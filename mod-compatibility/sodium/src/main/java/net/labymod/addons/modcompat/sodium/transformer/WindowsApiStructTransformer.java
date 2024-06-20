fae133076ba5583e5f956314288f475bfee5cf2c
Julian <42209193+juliarn@users.noreply.github.com>
2024-06-20 16:23:24 +0200

    Add transformer to support LWJGL 3.3.3 in Sodium 1.20.1

---
+++ mod-compatibility/sodium/src/main/java/net/labymod/addons/modcompat/sodium/transformer/WindowsApiStructTransformer.java
@@ -0,0 +1,69 @@
+package net.labymod.addons.modcompat.sodium.transformer;
+
+import net.labymod.api.loader.MinecraftVersions;
+import net.labymod.api.models.addon.annotation.EarlyAddonTransformer;
+import net.labymod.api.volt.asm.util.ASMHelper;
+import net.minecraft.launchwrapper.IClassTransformer;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.ClassNode;
+
+/**
+ * Sodium 1.20.1 expects LWJGL 3.3.1, but LabyMod always uses LWJGL 3.3.3. In between these
+ * versions, LWJGL broke their api, causing Sodium to crash with LWJGL 3.3.3. This transformer adds
+ * the required override for the new create method.
+ */
+@EarlyAddonTransformer
+public class WindowsApiStructTransformer implements IClassTransformer {
+
+  private static final String PACKAGE_NAME = "me.jellysquid.mods.sodium.client.platform.windows.api.";
+  private static final String LWJGL_STRUCT = "org/lwjgl/system/Struct";
+
+  private static final String CONSTRUCTOR_NAME = "<init>";
+  private static final String CONSTRUCTOR_DESC = "(JLjava/nio/ByteBuffer;)V";
+
+  private static final String CREATE_METHOD_NAME = "create";
+  private static final String CREATE_METHOD_DESC = "(JLjava/nio/ByteBuffer;)L%s;";
+
+  @Override
+  public byte[] transform(String name, String transformedName, byte... classBytes) {
+    if (name != null && name.startsWith(PACKAGE_NAME) && MinecraftVersions.V1_20_1.isCurrent()) {
+      return ASMHelper.transformClassData(classBytes, this::transform);
+    }
+    return classBytes;
+  }
+
+  private void transform(ClassNode classNode) {
+    if (!LWJGL_STRUCT.equals(classNode.superName)) {
+      return;
+    }
+
+    String className = classNode.name;
+
+    classNode.methods.add(ASMHelper.createMethod(
+        Opcodes.ACC_PROTECTED,
+        CREATE_METHOD_NAME,
+        String.format(CREATE_METHOD_DESC, className),
+        null,
+        null,
+        methodNode -> {
+          // Create new instance of the class
+          methodNode.visitTypeInsn(Opcodes.NEW, className);
+          methodNode.visitInsn(Opcodes.DUP);
+
+          // Call constructor with long (address) and ByteBuffer (container)
+          methodNode.visitVarInsn(Opcodes.LLOAD, 1);
+          methodNode.visitVarInsn(Opcodes.ALOAD, 3);
+          methodNode.visitMethodInsn(
+              Opcodes.INVOKESPECIAL,
+              className,
+              CONSTRUCTOR_NAME,
+              CONSTRUCTOR_DESC,
+              false
+          );
+
+          // Return the new instance
+          methodNode.visitInsn(Opcodes.ARETURN);
+        }
+    ));
+  }
+}
