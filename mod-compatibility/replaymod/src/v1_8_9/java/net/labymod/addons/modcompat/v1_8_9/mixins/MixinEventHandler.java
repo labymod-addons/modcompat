7d9e7e519791e3a24e7a963fafc98e5338b96715
Christian <r0bby@gmx.net>
2025-10-14 18:42:19 +0200

    Add `remap = false` to ReplayMod input event injections

--- mod-compatibility/replaymod/src/v1_8_9/java/net/labymod/addons/modcompat/v1_8_9/mixins/MixinEventHandler.java
+++ mod-compatibility/replaymod/src/v1_8_9/java/net/labymod/addons/modcompat/v1_8_9/mixins/MixinEventHandler.java
@@ -10,13 +10,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinEventHandler {
 
   @Dynamic
-  @Inject(method = "onMouseInput", at = @At("HEAD"), cancellable = true)
+  @Inject(method = "onMouseInput", at = @At("HEAD"), cancellable = true, remap = false)
   private void modcompat$disableMouseInput(CallbackInfo ci) {
     ci.cancel();
   }
 
   @Dynamic
-  @Inject(method = "onKeyboardInput", at = @At("HEAD"), cancellable = true)
+  @Inject(method = "onKeyboardInput", at = @At("HEAD"), cancellable = true, remap = false)
   private void modcompat$disableKeyboardInput(CallbackInfo ci) {
     ci.cancel();
   }
