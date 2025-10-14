b29f1735dc20fa588b5ee8c560898d7af8559a7b
Christian <r0bby@gmx.net>
2025-10-14 18:19:30 +0200

    Prevent ReplayMod from canceling mouse and keyboard actions

--- mod-compatibility/replaymod/build.gradle.kts
+++ mod-compatibility/replaymod/build.gradle.kts
@@ -1,8 +1,16 @@
 import net.labymod.labygradle.common.extension.LabyModAnnotationProcessorExtension.ReferenceType
+import net.labymod.labygradle.common.internal.fabric.dependency.ModrinthDependencyHandler
 
 dependencies {
     api(project(":core"))
-    compileOnly("maven.modrinth:replaymod:1.20.4-2.6.15")
+
+    for (sourceSet in sourceSets) {
+        if (sourceSet.name == "v1_8_9") {
+            add(sourceSet.compileOnlyConfigurationName, "maven.modrinth:replaymod:1.8.9-2.6.20")
+        } else {
+            add(sourceSet.compileOnlyConfigurationName, "maven.modrinth:replaymod:1.20.4-2.6.15")
+        }
+    }
 }
 
 labyModAnnotationProcessor {
---
+++ mod-compatibility/replaymod/src/v1_8_9/java/net/labymod/addons/modcompat/v1_8_9/mixins/MixinEventHandler.java
@@ -0,0 +1,24 @@
+package net.labymod.addons.modcompat.v1_8_9.mixins;
+
+import org.spongepowered.asm.mixin.Dynamic;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(targets = "com/replaymod/lib/de/johni0702/minecraft/gui/container/VanillaGuiScreen$EventHandler")
+public class MixinEventHandler {
+
+  @Dynamic
+  @Inject(method = "onMouseInput", at = @At("HEAD"), cancellable = true)
+  private void modcompat$disableMouseInput(CallbackInfo ci) {
+    ci.cancel();
+  }
+
+  @Dynamic
+  @Inject(method = "onKeyboardInput", at = @At("HEAD"), cancellable = true)
+  private void modcompat$disableKeyboardInput(CallbackInfo ci) {
+    ci.cancel();
+  }
+
+}
