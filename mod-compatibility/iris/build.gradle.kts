610255130c503185427b1ecd8a7a872c40359918
Christian <r0bby@gmx.net>
2025-11-20 15:30:47 +0100

    Fix iris

--- mod-compatibility/iris/build.gradle.kts
+++ mod-compatibility/iris/build.gradle.kts
@@ -1,9 +1,24 @@
-
-repositories {
-    maven("https://api.modrinth.com/maven")
-}
+import net.labymod.labygradle.common.internal.fabric.dependency.ModrinthDependencyHandler
+import org.gradle.kotlin.dsl.findByType
+import kotlin.apply
 
 dependencies {
     api(project(":core"))
     compileOnly("maven.modrinth:iris:1.4.5+1.16.5")
+
+    extensions.findByType(ModrinthDependencyHandler::class)?.apply {
+        this.modrinth("1.16.5", "iris", "1.4.5+1.16.5")
+        this.modrinth("1.17.1", "iris", "1.17.x-v1.2.6")
+        this.modrinth("1.18.2", "iris", "1.6.11+1.18.2")
+        this.modrinth("1.19.4", "iris", "1.6.11+1.19.4")
+        this.modrinth("1.20.1", "iris", "1.7.6+1.20.1")
+        this.modrinth("1.20.6", "iris", "1.7.2+1.20.6")
+        this.modrinth("1.21", "iris", "1.8.8+1.21.1-fabric")
+        this.modrinth("1.21.1", "iris", "1.8.8+1.21.1-fabric")
+        this.modrinth("1.21.3", "iris", "1.8.1+1.21.3-fabric")
+        this.modrinth("1.21.4", "iris", "1.8.8+1.21.4-fabric")
+        this.modrinth("1.21.5", "iris", "1.8.11+1.21.5-fabric")
+        this.modrinth("1.21.8", "iris", "1.9.6+1.21.8-fabric")
+        this.modrinth("1.21.10", "iris", "1.9.6+1.21.10-fabric")
+    }
 }
---
+++ mod-compatibility/iris/src/v1_20_6/java/net/labymod/addons/modcompat/v1_20_6/mixins/iris/MixinShadowRenderer.java
@@ -0,0 +1,35 @@
+package net.labymod.addons.modcompat.v1_20_6.mixins.iris;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.irisshaders.iris.mixin.LevelRendererAccessor;
+import net.irisshaders.iris.shadows.ShadowRenderer;
+import net.labymod.api.client.render.matrix.Stack;
+import net.labymod.api.client.render.matrix.VanillaStackAccessor;
+import net.labymod.core.event.client.render.world.RenderWorldEventCaller;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ShadowRenderer.class)
+public class MixinShadowRenderer {
+
+  @Inject(method = "renderShadows", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;endBatch()V", shift = Shift.AFTER))
+  private void labyMod$renderLabyModFeatures(
+      LevelRendererAccessor levelRenderer,
+      Camera playerCamera,
+      CallbackInfo ci,
+      @Local PoseStack poseStack
+  ) {
+    Stack stack = ((VanillaStackAccessor) poseStack).stack();
+    RenderWorldEventCaller.callPost(
+        stack,
+        Minecraft.getInstance().getFrameTime()
+    );
+  }
+
+}
