99c0022822f640c57cca8cbf9ebb0f35d75e0e09
Christian <r0bby@gmx.net>
2026-05-27 18:36:07 +0200

    Use @NullableReference on ModMenuAccessor

--- core/src/main/java/net/labymod/addons/modcompat/acessor/ModMenuAccessor.java
+++ core/src/main/java/net/labymod/addons/modcompat/acessor/ModMenuAccessor.java
@@ -3,11 +3,12 @@ package net.labymod.addons.modcompat.acessor;
 import net.labymod.api.Laby;
 import net.labymod.api.client.gui.screen.ScreenInstance;
 import net.labymod.api.client.gui.screen.ScreenWrapper;
+import net.labymod.api.models.NullableReference;
 import net.labymod.api.reference.annotation.Referenceable;
 import org.jetbrains.annotations.Nullable;
 
 // TODO: It should be possible that this class is in the mod-compatibility module
-@Nullable
+@NullableReference
 @Referenceable
 public interface ModMenuAccessor {
 
